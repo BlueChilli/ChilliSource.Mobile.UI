2f9fa07e90aebaa0c245d9b4bef104f74ea9c5df
Naoto Onishi <onikiri@Naotos-MacBook-Pro.local>
2017-05-03 17:26:22 +1000

    added ui test for styled navigation bar

--- src/ChilliSource.Mobile.Examples/UITests/ChilliSource.Mobile.UI/StyledNavigationTests.cs
+++ src/ChilliSource.Mobile.Examples/UITests/ChilliSource.Mobile.UI/StyledNavigationTests.cs
@@ -26,10 +26,27 @@ namespace Examples.UITests
 		[Test]
 		public void StyledNavigationBarPage_ShouldChangeBasedOnSubtitleProperty()
 		{
+            app.Tap(x => x.Marked("Let's go!"));
 			app.Tap(x => x.Id("Images/Index/Behaviours"));
             app.Tap(x => x.Marked("Style that nav bar!"));
             app.WaitForElement(x => x.Text("Hide Left Toolbar"));
-            app.Query();
+            Assert.AreEqual(1, app.Query(x => x.Marked("title")).Length);
+            app.Tap(x => x.Text("ChangeSubTitle"));
+            Assert.AreEqual(1,app.Query(x => x.Marked("SubTitle changed")).Length);
+		}
+
+        [Test]
+        public void StyledNavigationBarPage_CanChangeVisibilityOfToolbarItem()
+        {
+            app.Tap(x => x.Marked("Let's go!"));
+            app.Tap(x => x.Id("Images/Index/Behaviours"));
+            app.Tap(x => x.Marked("Style that nav bar!"));
+            app.WaitForElement(x => x.Text("Hide Left Toolbar"));
+            Assert.AreEqual(1, app.Query(x => x.Text("Right Item")).Length);
+            app.Tap(x => x.Text("Hide Right Toolbar"));
+			Assert.AreEqual(0, app.Query(x => x.Text("Right Item")).Length);
+			app.Tap(x => x.Text("Show Right Toolbar"));
+			Assert.AreEqual(1, app.Query(x => x.Text("Right Item")).Length);
 		}
     }
 }
